abe28af59a8ac088970f6e9c1396ca893feb44aa
sakilk130 <sakilk130@gmail.com>
2020-08-30 00:20:00 +0600

    Third Commit

--- app.js
+++ app.js
@@ -3,15 +3,34 @@ var budgetController = (function () {
 })();
 
 var UIController = (function () {
-  //
+  var DOMstring = {
+    inputType: ".add__type",
+    addDescription: ".add__description",
+    addValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstring.inputType).value,
+        description: document.querySelector(DOMstring.addDescription).value,
+        value: document.querySelector(DOMstring.addValue).value,
+      };
+    },
+    getDOMstring: function () {
+      return DOMstring;
+    },
+  };
 })();
 
-var appController = (function (budgetCont, uiCont) {
+var appController = (function (budgetCont, UICont) {
+  var DOM = UICont.getDOMstring();
   var controlAddItem = function () {
-    console.log("It works");
+    var input = UICont.getInput();
+    console.log(input);
   };
 
-  document.querySelector(".add__btn").addEventListener("click", controlAddItem);
+  document.querySelector(DOM.addBtn).addEventListener("click", controlAddItem);
 
   document.addEventListener("keypress", function (event) {
     if (event === 13 || event.which === 13) {
