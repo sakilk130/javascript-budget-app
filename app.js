729a246b54a52c01b5be4bae138cd0b04a752a41
sakilk130 <sakilk130@gmail.com>
2020-09-03 11:29:07 +0600

    event delegation

--- app.js
+++ app.js
@@ -81,6 +81,7 @@ var UIController = (function () {
     incomeLabel: ".budget__income--value",
     expensesLabel: ".budget__expenses--value",
     percentageLabel: ".budget__expenses--percentage",
+    container: ".container",
   };
 
   return {
@@ -96,11 +97,11 @@ var UIController = (function () {
       if (type === "inc") {
         element = DOMstring.incomeContainer;
         html =
-          '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+          '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       } else {
         element = DOMstring.expensesContainer;
         html =
-          '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div> <div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+          '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div> <div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
       newHtml = html.replace("%id%", obj.id);
       newHtml = newHtml.replace("%description%", obj.description);
@@ -149,6 +150,9 @@ var appController = (function (budgetCont, UICont) {
         controlAddItem();
       }
     });
+    document
+      .querySelector(DOM.container)
+      .addEventListener("click", controlDeleteItem);
   };
   var updateBudget = function () {
     budgetCont.calculateBudget();
@@ -166,7 +170,15 @@ var appController = (function (budgetCont, UICont) {
       updateBudget();
     }
   };
-
+  var controlDeleteItem = function (event) {
+    var itemID, splitID, type, ID;
+    itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+    if (itemID) {
+      splitID = itemID.split("-");
+      type = splitID[0];
+      ID = splitID[1];
+    }
+  };
   return {
     init: function () {
       console.log("App Start");
